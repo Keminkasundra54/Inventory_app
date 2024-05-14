3a76fcaf40c4d2ca4e84878719022b19ee8e8b4b
python1621 <kemin@hubresolution.com~>
2024-05-14 18:06:43 +0530

    Refactored authservice

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -13,12 +13,14 @@ export class AuthService {
   userData = signal('')
   menuSignal = signal(false)
   constructor(private http: HttpClient, private router: Router) {
-    effect(()=>{
+    effect(() => {
       this.getToken()
     })
 
   }
   signup(data: any) {
+    console.log(data);
+
     let url = this.url() + 'register'
 
     let info = this.http.post(url, data, { observe: 'response' })
@@ -27,11 +29,13 @@ export class AuthService {
     return info
   }
   login(data: any) {
+    console.log(data);
+
     let url = this.url() + 'login'
 
     let info = this.http.post(url, data, { observe: 'response' })
     console.log(info);
-    
+
     return info
   }
   forgotPassword(data: any) {
@@ -61,7 +65,7 @@ export class AuthService {
   getToken() {
     if (localStorage.getItem('userData')) {
       console.log(JSON.parse(localStorage.getItem('userData')!).token);
-      
+
       return JSON.parse(localStorage.getItem('userData')!).token
     } else {
       this.router.navigate(['']);
